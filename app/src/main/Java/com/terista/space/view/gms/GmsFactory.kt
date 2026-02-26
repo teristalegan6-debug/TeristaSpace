fd0045c1b0298e3e9af6b8c748d3670ba90ce2f7
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-26 17:43:43 +0530

    Add files via upload

---
+++ app/src/main/Java/com/terista/space/view/gms/GmsFactory.kt
@@ -0,0 +1,13 @@
+package com.terista.space.view.gms
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.terista.space.data.GmsRepository
+
+
+class GmsFactory(private val repo:GmsRepository): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return GmsViewModel(repo) as T
+    }
+}
