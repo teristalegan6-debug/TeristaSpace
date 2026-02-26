66dcf240f47e1d139ff7b57cbdb6abc300a598e2
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 02:26:45 +0530

    Add files via upload

---
+++ Bcore/src/main/java/com/terista/space/core/fake/delegate/AppInstrumentation.java
@@ -0,0 +1,150 @@
+package com.terista.space.core.fake.delegate;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+import black.android.app.BRActivity;
+import black.android.app.BRActivityThread;
+import com.terista.space.core.BlackBoxCore;
+import com.terista.space.core.app.BActivityThread;
+import com.terista.space.core.fake.hook.HookManager;
+import com.terista.space.core.fake.hook.IInjectHook;
+import com.terista.space.core.fake.service.HCallbackProxy;
+import com.terista.space.core.fake.service.IActivityClientProxy;
+import com.terista.space.core.utils.HackAppUtils;
+import com.terista.space.core.utils.compat.ActivityCompat;
+import com.terista.space.core.utils.compat.ActivityManagerCompat;
+import com.terista.space.core.utils.compat.ContextCompat;
+
+public final class AppInstrumentation extends BaseInstrumentationDelegate implements IInjectHook {
+
+    private static final String TAG = AppInstrumentation.class.getSimpleName();
+
+    private static AppInstrumentation sAppInstrumentation;
+
+    public static AppInstrumentation get() {
+        if (sAppInstrumentation == null) {
+            synchronized (AppInstrumentation.class) {
+                if (sAppInstrumentation == null) {
+                    sAppInstrumentation = new AppInstrumentation();
+                }
+            }
+        }
+        return sAppInstrumentation;
+    }
+
+    public AppInstrumentation() {
+    }
+
+    @Override
+    public void injectHook() {
+        try {
+            Instrumentation mInstrumentation = getCurrInstrumentation();
+            if (mInstrumentation == this || checkInstrumentation(mInstrumentation))
+                return;
+            mBaseInstrumentation = (Instrumentation) mInstrumentation;
+            BRActivityThread.get(BlackBoxCore.mainThread())._set_mInstrumentation(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Instrumentation getCurrInstrumentation() {
+        Object currentActivityThread = BlackBoxCore.mainThread();
+        return BRActivityThread.get(currentActivityThread).mInstrumentation();
+    }
+
+    @Override
+    public boolean isBadEnv() {
+        return !checkInstrumentation(getCurrInstrumentation());
+    }
+
+    private boolean checkInstrumentation(Instrumentation instrumentation) {
+        if (instrumentation instanceof AppInstrumentation) {
+            return true;
+        }
+        Class<?> clazz = instrumentation.getClass();
+        if (Instrumentation.class.equals(clazz)) {
+            return false;
+        }
+        do {
+            assert clazz != null;
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if (Instrumentation.class.isAssignableFrom(field.getType())) {
+                    field.setAccessible(true);
+                    try {
+                        Object obj = field.get(instrumentation);
+                        if ((obj instanceof AppInstrumentation)) {
+                            return true;
+                        }
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }
+            }
+            clazz = clazz.getSuperclass();
+        } while (!Instrumentation.class.equals(clazz));
+        return false;
+    }
+
+    private void checkHCallback() {
+        HookManager.get().checkEnv(HCallbackProxy.class);
+    }
+
+    private void checkActivity(Activity activity) {
+        Log.d(TAG, "callActivityOnCreate: " + activity.getClass().getName());
+        HackAppUtils.enableQQLogOutput(activity.getPackageName(), activity.getClassLoader());
+        checkHCallback();
+        HookManager.get().checkEnv(IActivityClientProxy.class);
+        ActivityInfo info = BRActivity.get(activity).mActivityInfo();
+        ContextCompat.fix(activity);
+        ActivityCompat.fix(activity);
+        if (info.theme != 0) {
+            activity.getTheme().applyStyle(info.theme, true);
+        }
+        ActivityManagerCompat.setActivityOrientation(activity, info.screenOrientation);
+    }
+
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        ContextCompat.fix(context);
+
+        return super.newApplication(cl, className, context);
+    }
+
+    @Override
+    public void callActivityOnCreate(Activity activity, Bundle icicle, PersistableBundle persistentState) {
+        checkActivity(activity);
+        super.callActivityOnCreate(activity, icicle, persistentState);
+    }
+
+    @Override
+    public void callActivityOnCreate(Activity activity, Bundle icicle) {
+        checkActivity(activity);
+        super.callActivityOnCreate(activity, icicle);
+    }
+
+    @Override
+    public void callApplicationOnCreate(Application app) {
+        checkHCallback();
+        super.callApplicationOnCreate(app);
+    }
+
+    public Activity newActivity(ClassLoader cl, String className, Intent intent) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        try {
+            return super.newActivity(cl, className, intent);
+        } catch (ClassNotFoundException e) {
+            return mBaseInstrumentation.newActivity(cl, className, intent);
+        }
+    }
+}
