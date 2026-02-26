652b6fda26ee06f156130ede3fae85d94c36b226
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-26 17:37:36 +0530

    Add files via upload

---
+++ app/src/main/Java/com/terista/space/view/base/BaseViewModel.kt
@@ -0,0 +1,29 @@
+package com.terista.space.view.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+
+
+open class BaseViewModel : ViewModel() {
+
+    fun launchOnUI(block: suspend CoroutineScope.() -> Unit) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                try {
+                    block()
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelScope.cancel()
+    }
+
+}
