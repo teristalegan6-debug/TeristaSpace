e7a84934faf8430f8006fd7b329046e2e9cf1cfe
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-26 17:25:42 +0530

    Add files via upload

---
+++ app/src/main/Java/com/terista/space/bean/FakeLocationBean.kt
@@ -0,0 +1,15 @@
+package com.terista.space.bean
+
+import android.graphics.drawable.Drawable
+import com.terista.space.core.entity.location.BLocation
+
+data class FakeLocationBean(
+    val userID: Int,
+    val name: String,
+    val icon: Drawable,
+    val packageName: String,
+    var fakeLocationPattern: Int,
+    var fakeLocation: BLocation?
+)
+
+data class FakeLocationBeanInstallBean(val userID: Int, val success: Boolean, val msg: String)
---
+++ app/src/main/Java/com/terista/space/bean/InstalledAppBean.kt
@@ -0,0 +1,6 @@
+package com.terista.space.bean
+
+import android.graphics.drawable.Drawable
+
+
+data class InstalledAppBean(val name:String, val icon: Drawable?, val packageName:String, val sourceDir:String, val isInstall:Boolean)
