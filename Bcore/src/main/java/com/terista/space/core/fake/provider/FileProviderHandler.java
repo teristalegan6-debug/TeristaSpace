73e959ef159afd7a38347a65ea01f698278ca00f
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 02:29:13 +0530

    Add files via upload

---
+++ Bcore/src/main/java/com/terista/space/core/fake/provider/FileProviderHandler.java
@@ -0,0 +1,40 @@
+package com.terista.space.core.fake.provider;
+
+import android.content.Context;
+import android.content.pm.ProviderInfo;
+import android.net.Uri;
+
+import java.io.File;
+import java.util.List;
+
+import com.terista.space.core.BlackBoxCore;
+import com.terista.space.core.app.BActivityThread;
+import com.terista.space.core.utils.compat.BuildCompat;
+
+
+public class FileProviderHandler {
+
+    public static Uri convertFileUri(Context context, Uri uri) {
+        if (BuildCompat.isN()) {
+            File file = convertFile(context, uri);
+            if (file == null)
+                return null;
+            return BlackBoxCore.getBStorageManager().getUriForFile(file.getAbsolutePath());
+        }
+        return uri;
+    }
+
+    public static File convertFile(Context context, Uri uri) {
+        List<ProviderInfo> providers = BActivityThread.getProviders();
+        for (ProviderInfo provider : providers) {
+            try {
+                File fileForUri = FileProvider.getFileForUri(context, provider.authority, uri);
+                if (fileForUri != null && fileForUri.exists()) {
+                    return fileForUri;
+                }
+            } catch (Exception ignored) {
+            }
+        }
+        return null;
+    }
+}
