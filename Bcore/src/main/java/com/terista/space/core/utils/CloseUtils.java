65638263a3e49751a8d510d0150f05a3d48bc584
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 02:48:15 +0530

    Add files via upload

---
+++ Bcore/src/main/java/com/terista/space/core/utils/CloseUtils.java
@@ -0,0 +1,21 @@
+package com.terista.space.core.utils;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+
+public class CloseUtils {
+    public static void close(Closeable... closeables) {
+        if (closeables == null) {
+            return;
+        }
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}
