66852dd3001f27b345add8a7ba5fa5e3021d9262
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 00:55:33 +0530

    Add files via upload

---
+++ black-reflection/src/main/java/com/terista/space/reflection/BlackNullPointerException.java
@@ -0,0 +1,8 @@
+package com.terista.space.reflection;
+
+
+public class BlackNullPointerException extends NullPointerException {
+    public BlackNullPointerException(String s) {
+        super(s);
+    }
+}
