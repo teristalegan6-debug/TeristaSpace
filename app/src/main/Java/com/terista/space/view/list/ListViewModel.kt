b97cb1736a5ebc3675a236b3582eb301c1c69a5a
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-26 17:43:09 +0530

    Add files via upload

---
+++ app/src/main/Java/com/terista/space/view/list/ListViewModel.kt
@@ -0,0 +1,22 @@
+package com.terista.space.view.list
+
+import androidx.lifecycle.MutableLiveData
+import com.terista.space.bean.InstalledAppBean
+import com.terista.space.data.AppsRepository
+import com.terista.space.view.base.BaseViewModel
+
+
+class ListViewModel(private val repo: AppsRepository) : BaseViewModel() {
+
+    val appsLiveData = MutableLiveData<List<InstalledAppBean>>()
+
+    val loadingLiveData = MutableLiveData<Boolean>()
+
+    fun previewInstalledList() {
+        launchOnUI { repo.previewInstallList() }
+    }
+
+    fun getInstallAppList(userID: Int) {
+        launchOnUI { repo.getInstalledAppList(userID, loadingLiveData, appsLiveData) }
+    }
+}
