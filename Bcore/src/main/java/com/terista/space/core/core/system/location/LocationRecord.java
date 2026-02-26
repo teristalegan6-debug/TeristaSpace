46028ce3bd7a5841763918dbc536fc991e3ebc48
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 02:07:04 +0530

    Add files via upload

---
+++ Bcore/src/main/java/com/terista/space/core/core/system/location/LocationRecord.java
@@ -0,0 +1,12 @@
+package com.terista.space.core.core.system.location;
+
+
+public class LocationRecord {
+    public String packageName;
+    public int userId;
+
+    public LocationRecord(String packageName, int userId) {
+        this.packageName = packageName;
+        this.userId = userId;
+    }
+}
