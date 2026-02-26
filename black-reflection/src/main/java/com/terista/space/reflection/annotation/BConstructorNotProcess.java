c2db3fb30b00b1a5b49515e0e5371eb483fd60e9
teristalegan6-debug <teristalegan6@gmail.com>
2026-02-27 00:57:44 +0530

    Add files via upload

---
+++ black-reflection/src/main/java/com/terista/space/reflection/annotation/BConstructorNotProcess.java
@@ -0,0 +1,14 @@
+package com.terista.space.reflection.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Retention(RUNTIME)
+@Target({METHOD})
+public @interface BConstructorNotProcess {
+
+}
---
+++ black-reflection/src/main/java/com/terista/space/reflection/annotation/BParamClass.java
@@ -0,0 +1,15 @@
+package com.terista.space.reflection.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Retention(RUNTIME)
+@Target({PARAMETER})
+public @interface BParamClass {
+    Class<?> value();
+}
